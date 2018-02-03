7ae3bdf76af57ee14381303f95854cab80704579
cpkurotori <ckurotori2@ohlone.edu>
2018-02-03 00:02:12 -0800

    clean start

--- classes.py
+++
@@ -1,7 +0,0 @@
-class Entry:
-    def __init__(self,dept,inTime,outTime,duration="",flag=0):
-        self.dept = dept
-        self.inTime = inTime
-        self.outTime = outTime
-        self.duration = duration
-        self.flag = flag
--- static/assets/js/timecard.js
+++
@@ -1,9 +0,0 @@
-function addEntry(){
-    var table = document.getElementById('timecard-table');
-    var row = document.createElement('TR')
-    var date = document.createElement('TD')
-    var dept = document.createElement('TD')
-    var clockIn = document.createElement('TD')
-    var clockOut = document.createElement('TD')
-    var hours = document.createElement('TD')
-}
